04abdfa7ff4aa08468f63ee0e84fd80a7fe2e86f
omar ezzdeen <omar.izziddeen@gmail.com>
2022-08-24 08:10:41 +0300

    create navigation components

---
+++ app/src/main/java/com/example/onemillonwinner/fragments/ResultFragment.kt
@@ -0,0 +1,17 @@
+package com.example.onemillonwinner.fragments
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.onemillonwinner.databinding.FragmentResultBinding
+
+class ResultFragment : BaseFragment<FragmentResultBinding>() {
+
+    override var LOG_TAG = "RESULT_FRAGMENT"
+
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentResultBinding
+        get() = FragmentResultBinding::inflate
+
+    override fun addCallBacks() {
+        TODO("Not yet implemented")
+    }
+}
