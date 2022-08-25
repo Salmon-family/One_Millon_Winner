e54af31e47b98ae5fb414f5dfbf0234df452d2b8
AmeerAmjed <pr.ameer98@gmail.com>
2022-08-25 23:19:47 +0300

    create base fragment with view data binding

---
+++ app/src/main/java/com/example/onemillonwinner/ui/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.example.onemillonwinner.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VDB : ViewDataBinding> : Fragment() {
+
+    abstract val layoutIdFragment: Int
+
+    private lateinit var _binding: VDB
+    protected val binding: VDB
+        get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = DataBindingUtil.inflate<VDB>(inflater, layoutIdFragment, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        return _binding.root
+    }
+
+}
