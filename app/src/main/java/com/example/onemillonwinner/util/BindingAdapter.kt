cc5c1fd72cc8d4a13747467ee354cfe8f8456bf2
Amnah_ <amnah44ali@gmail.com>
2022-08-26 19:04:18 +0300

    handle raw answers colors

--- app/src/main/java/com/example/onemillonwinner/util/BindingAdapter.kt
+++ app/src/main/java/com/example/onemillonwinner/util/BindingAdapter.kt
@@ -4,13 +4,28 @@ import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.onemillonwinner.R
+import com.example.onemillonwinner.data.enum.SelectAnswer
 
 @BindingAdapter(value = ["disableButton"])
-fun disableButton(view: View, value: Boolean?){
-    if(value == true){
+fun disableButton(view: View, value: Boolean?) {
+    if (value == true) {
         view.setBackgroundColor(ContextCompat.getColor(view.context, R.color.red_200))
         view.isClickable = false
-    }else{
+    } else {
         view.setBackgroundColor(ContextCompat.getColor(view.context, R.color.blue_200))
+    }
+}
+
+@BindingAdapter(value = ["rawAnswersColor"])
+fun rawAnswersColor(view: View, selectedAnswer: SelectAnswer?) {
+    when (selectedAnswer) {
+        SelectAnswer.UNSELECTED_ANSWER -> view.background = ContextCompat
+            .getDrawable(view.context, R.drawable.raw_unselected_answers_shape)
+        SelectAnswer.CORRECT_ANSWER -> view.background = ContextCompat
+            .getDrawable(view.context, R.drawable.raw_correct_answers_shape)
+        SelectAnswer.INCORRECT_ANSWER -> view.background = ContextCompat
+            .getDrawable(view.context, R.drawable.raw_incorrect_answers_shape)
+        else -> view.background = ContextCompat
+            .getDrawable(view.context, R.drawable.raw_unselected_answers_shape)
     }
 }
