647b3fc06354cdcb95253cd52d5c4f8a27737e51
omar ezzdeen <omar.izziddeen@gmail.com>
2022-08-26 14:11:04 +0300

    create question timer

--- app/src/main/java/com/example/onemillonwinner/ui/fragments/game/GameFragment.kt
+++ app/src/main/java/com/example/onemillonwinner/ui/fragments/game/GameFragment.kt
@@ -1,6 +1,11 @@
 package com.example.onemillonwinner.ui.fragments.game
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.os.CountDownTimer
+import android.view.animation.DecelerateInterpolator
 import androidx.fragment.app.viewModels
+import androidx.navigation.Navigation
 import com.example.onemillonwinner.R
 import com.example.onemillonwinner.databinding.FragmentGameBinding
 import com.example.onemillonwinner.ui.base.BaseFragment
@@ -10,9 +15,55 @@ class GameFragment : BaseFragment<FragmentGameBinding>() {
     private val gameViewModel: GameViewModel by viewModels()
 
 
+    override val layoutIdFragment = R.layout.fragment_game
+
     override fun setup() {
         binding.gameViewModel = gameViewModel
+        questionTimer(10000)
     }
 
-    override val layoutIdFragment = R.layout.fragment_game
+    private fun questionTimer(timeInMilliseconds: Long) {
+        timerProgressBar(timeInMilliseconds)
+        timerText(timeInMilliseconds)
+    }
+
+    private fun timerProgressBar(timeTimer: Long) {
+        val animation = ObjectAnimator.ofInt(binding.progressBar, "progress", 100, 0)
+        animation.duration = timeTimer
+        animation.interpolator = DecelerateInterpolator()
+        animation.addListener(object : Animator.AnimatorListener {
+
+            override fun onAnimationStart(animator: Animator) {}
+
+            override fun onAnimationEnd(animator: Animator) {
+                //do something when the countdown is complete
+            }
+
+            override fun onAnimationCancel(animator: Animator) {}
+
+            override fun onAnimationRepeat(animator: Animator) {}
+        })
+        animation.start()
+    }
+
+    private fun timerText(timeTimer: Long) {
+        object : CountDownTimer(timeTimer, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                binding.texTimer.text = "${millisUntilFinished / 1000}"
+                // logic to set the EditText could go here
+            }
+
+            override fun onFinish() {
+                //do something when the countdown is complete
+
+                endTheGame()
+            }
+        }.start()
+    }
+
+
+    private fun endTheGame() {
+        Navigation.findNavController(binding.root)
+            .navigate(GameFragmentDirections.actionGameFragmentToResultFragment())
+    }
 }
