eba1786daf8a9cad1dc5b296ee5aae846de891cd
omar ezzdeen <omar.izziddeen@gmail.com>
2022-08-26 23:30:04 +0300

    edit some names

--- app/src/main/java/com/example/onemillonwinner/ui/fragments/game/GameBindingAdapter.kt
+++ app/src/main/java/com/example/onemillonwinner/ui/fragments/game/GameBindingAdapter.kt
@@ -12,9 +12,9 @@ import androidx.databinding.BindingAdapter
 
 @SuppressLint("ObjectAnimatorBinding")
 @BindingAdapter(value = ["app:timerProgressBar"])
-fun timerProgressBar(view: ProgressBar, timeMill: Long) {
-    val animation = ObjectAnimator.ofInt(view, "progress", 100, 0)
-    animation.duration = timeMill
+fun timerProgressBar(progressBar: ProgressBar, timeMillisecond: Long) {
+    val animation = ObjectAnimator.ofInt(progressBar, "progress", 100, 0)
+    animation.duration = timeMillisecond
     animation.interpolator = DecelerateInterpolator()
     animation.addListener(object : Animator.AnimatorListener {
 
@@ -32,8 +32,8 @@ fun timerProgressBar(view: ProgressBar, timeMill: Long) {
 }
 
 @BindingAdapter(value = ["app:timerText"])
-fun timerText(textView: TextView, timeMill: Long) {
-    object : CountDownTimer(timeMill, 1000) {
+fun timerText(textView: TextView, timeMillisecond: Long) {
+    object : CountDownTimer(timeMillisecond, 1000) {
         override fun onTick(millisUntilFinished: Long) {
             textView.text = "${millisUntilFinished / 1000}"
             // logic to set the EditText could go here
