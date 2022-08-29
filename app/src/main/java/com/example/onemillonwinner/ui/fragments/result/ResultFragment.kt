ad7b129e950daf0543c514dd9bc8cde5b6ee3815
omar ezzdeen <omar.izziddeen@gmail.com>
2022-08-29 15:48:32 +0300

    create result screen navigations

--- app/src/main/java/com/example/onemillonwinner/ui/fragments/result/ResultFragment.kt
+++ app/src/main/java/com/example/onemillonwinner/ui/fragments/result/ResultFragment.kt
@@ -1,16 +1,31 @@
 package com.example.onemillonwinner.ui.fragments.result
 
 
+import android.annotation.SuppressLint
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.onemillonwinner.R
 import com.example.onemillonwinner.databinding.FragmentResultBinding
 import com.example.onemillonwinner.ui.base.BaseFragment
 
 class ResultFragment : BaseFragment<FragmentResultBinding>() {
 
+    override val layoutIdFragment = R.layout.fragment_result
 
     override fun setup() {
-
+        goToGameFragment()
+        goToHomeFragment()
     }
 
-    override val layoutIdFragment = R.layout.fragment_result
+    private fun goToGameFragment() {
+        binding.buttonPlayAgain.setOnClickListener {
+            it.findNavController().navigate(R.id.gameFragment)
+        }
+    }
+
+    private fun goToHomeFragment() {
+        binding.textBackHome.setOnClickListener {
+            it.findNavController().popBackStack(R.id.homeFragment, true)
+        }
+    }
 }
