2d993ad9fdaac2f65530eab677efa717a0ca6ac9
Nada Feteiha <nada.feteiha@gmail.com>
2022-08-30 22:12:48 -0700

    send argument to result and display it.

--- app/src/main/java/com/example/onemillonwinner/ui/fragments/result/ResultFragment.kt
+++ app/src/main/java/com/example/onemillonwinner/ui/fragments/result/ResultFragment.kt
@@ -1,16 +1,24 @@
 package com.example.onemillonwinner.ui.fragments.result
 
 
+import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.onemillonwinner.R
 import com.example.onemillonwinner.databinding.FragmentResultBinding
 import com.example.onemillonwinner.ui.base.BaseFragment
+import com.example.onemillonwinner.ui.fragments.game.GameViewModel
 
 class ResultFragment : BaseFragment<FragmentResultBinding>() {
 
     override val layoutIdFragment = R.layout.fragment_result
+    private val arguments: ResultFragmentArgs by navArgs()
+    private val resultViewModel: ResultViewModel by viewModels()
 
     override fun setup() {
+        val prize = arguments.prize
+        binding.prize = prize
+
         navigateToGameFragment()
         navigateToHomeFragment()
     }
--- app/src/main/java/com/example/onemillonwinner/ui/fragments/result/ResultViewModel.kt
+++ app/src/main/java/com/example/onemillonwinner/ui/fragments/result/ResultViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.onemillonwinner.ui.fragments.result
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ResultViewModel: ViewModel() {
+class ResultViewModel : ViewModel() {
+
+    private val _prize = MutableLiveData<String>()
+    val prize: LiveData<String>
+        get() = _prize
+
+
 }
