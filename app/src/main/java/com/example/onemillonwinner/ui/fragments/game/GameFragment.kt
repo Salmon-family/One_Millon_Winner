d28c8ee6ba5d367c73ee78d1764fc2dfd58bd81d
Amnah_ <amnah44ali@gmail.com>
2022-09-01 22:20:19 +0300

    add alert dialog in game fragment

--- app/src/main/java/com/example/onemillonwinner/ui/fragments/game/GameFragment.kt
+++ app/src/main/java/com/example/onemillonwinner/ui/fragments/game/GameFragment.kt
@@ -1,7 +1,9 @@
 package com.example.onemillonwinner.ui.fragments.game
 
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.onemillonwinner.R
 import com.example.onemillonwinner.data.GameState
@@ -12,6 +14,34 @@ class GameFragment : BaseFragment<FragmentGameBinding>() {
 
     private val gameViewModel: GameViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher
+            .addCallback(this, object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    showAlertDialog()
+                }
+            })
+    }
+
+    private fun showAlertDialog() {
+        val dialogBuilder = AlertDialog.Builder(requireContext())
+
+        dialogBuilder.apply {
+            setTitle(R.string.dialog_title)
+            setMessage(R.string.dialog_message)
+            setIcon(R.drawable.ic_alert_icon)
+            setPositiveButton(R.string.yes) { _, _ ->
+                findNavController().popBackStack()
+            }
+            setNegativeButton(R.string.no) { p0, _ ->
+                p0.cancel()
+            }
+        }.create().show()
+    }
+
+
     override fun setup() {
         binding.gameViewModel = gameViewModel
 
@@ -19,7 +49,7 @@ class GameFragment : BaseFragment<FragmentGameBinding>() {
     }
 
     private fun observeOnGameDone() {
-        gameViewModel.state.observe(viewLifecycleOwner, Observer {
+        gameViewModel.state.observe(viewLifecycleOwner) {
             it?.let {
                 if (it == GameState.GameOver) {
                     gameViewModel.prize.value?.let { prize ->
@@ -29,7 +59,7 @@ class GameFragment : BaseFragment<FragmentGameBinding>() {
                     }
                 }
             }
-        })
+        }
     }
 
     override val layoutIdFragment = R.layout.fragment_game
