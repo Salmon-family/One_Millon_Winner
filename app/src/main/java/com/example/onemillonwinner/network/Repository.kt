a4f5481bf27436b795dee1359a20ed70069d0f20
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-09-04 16:33:33 +0300

    rename NetworkConstants to ApiConstants

--- app/src/main/java/com/example/onemillonwinner/network/Repository.kt
+++ app/src/main/java/com/example/onemillonwinner/network/Repository.kt
@@ -2,11 +2,9 @@ package com.example.onemillonwinner.network
 
 import com.example.onemillonwinner.data.State
 import com.example.onemillonwinner.data.questionResponse.TriviaResponse
-import com.example.onemillonwinner.util.NetworkConstants.NUMBER_OF_QUESTIONS_PER_REQUEST
+import com.example.onemillonwinner.util.ApiConstants.NUMBER_OF_QUESTIONS_PER_REQUEST
 import com.example.onemillonwinner.util.enum.QuestionLevel
-import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.functions.BiFunction
 import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Response
