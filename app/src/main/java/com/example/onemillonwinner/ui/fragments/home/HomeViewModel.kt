d1c81f19f5d51e40f3cf172d7f86ed012fa13fc7
Karrar-Mohammed <karrar.m.habeeb@gmail.com>
2022-09-08 00:20:46 +0300

    rearrange home viewModel code

--- app/src/main/java/com/example/onemillonwinner/ui/fragments/home/HomeViewModel.kt
+++ app/src/main/java/com/example/onemillonwinner/ui/fragments/home/HomeViewModel.kt
@@ -9,12 +9,12 @@ import com.example.onemillonwinner.util.Event
 
 class HomeViewModel : BaseViewModel() {
     private val repository = Repository()
-    private val _bestPrize = MutableLiveData<Int>(repository.getBestPrize())
 
     private val _navigateToGameFragment = MutableLiveData(Event(false))
     val navigateToGameFragment: LiveData<Event<Boolean>>
         get() = _navigateToGameFragment
 
+    private val _bestPrize = MutableLiveData<Int>(repository.getBestPrize())
     val bestPrize: LiveData<Int>
         get() = _bestPrize
 
